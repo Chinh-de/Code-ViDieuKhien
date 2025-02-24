bd9cb56d76bb9df46b9f99a1e5b6fe8fe6d7b560
ChinhDe <chinhde29@gmail.com>
2025-02-24 15:51:48 +0700

    24/02

--- cambiendoam/cambiendoam.ino
+++ cambiendoam/cambiendoam.ino
@@ -2,20 +2,24 @@
 
 #define DHT11_PIN 4
 #define DHTTYPE DHT11
+int ALARM = 6; // Định nghĩa chân Buzzer là chân số 4
+float sinVal; 
+int toneVal;
 
 DHT dht11(DHT11_PIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
   dht11.begin();
+  pinMode(ALARM, OUTPUT);
   Serial.println("DHT11 TEST PROGRAM");
   Serial.println();
   Serial.println("Type\tStatus\tHumidity (%)\tTemperature (\"C\")");
 }
 
 void loop() {
-  delay(400);
-  
+//  delay(400);
+//  
   float h = dht11.readHumidity();
   float t = dht11.readTemperature();
   
@@ -29,4 +33,15 @@ void loop() {
   Serial.print("%\t\t");
   Serial.print(t);
   Serial.println("°C");
+
+  if(h>=75){
+    for (int x = 0; x < 180; x++) { 
+            sinVal = sin(x * (3.1412 / 180.0)); // Chuyển đổi sin thành radian
+            toneVal = 2000 + (int)(sinVal * 1000); // Sử dụng sin để tạo tần số âm thanh
+            tone(ALARM, toneVal); 
+            delay(2);
+        }
+  } else { 
+        noTone(ALARM);
+  }
 }
--- cambiendoline/cambiendoline.ino
+++ cambiendoline/cambiendoline.ino
@@ -1,6 +1,14 @@
+//phản xạ line trắng
+//hấp thụ là line đen, ko có gì là line đen, or hấp thụ hết gần như hoàn toàn là line đen
+
 int LINE = 2;
 int val = 0;
 int led = 13;
+
+//int ALARM = 6; // Định nghĩa chân Buzzer là chân số 4
+//float sinVal; 
+//int toneVal;
+
 
 void setup() {
     pinMode(led, OUTPUT);
@@ -16,4 +24,15 @@ void loop() {
     } else {
         digitalWrite(led, LOW);   // Tắt đèn LED
     }
+
+//    if(val == 1){
+//    for (int x = 0; x < 180; x++) { 
+//            sinVal = sin(x * (3.1412 / 180.0)); // Chuyển đổi sin thành radian
+//            toneVal = 2000 + (int)(sinVal * 1000); // Sử dụng sin để tạo tần số âm thanh
+//            tone(ALARM, toneVal); 
+//            delay(2);
+//        }
+//  } else { 
+//        noTone(ALARM);
+//  }
 }
